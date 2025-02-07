b2731ab7db6e0747b257c0a20f1736de3df3aaa1
zihadulIslam2 <zihadul708@gmail.com>
2025-02-07 12:17:34 +0600

    Implement email verification process with JWT token handling and update user routes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -34,18 +34,23 @@ const userSignup = async (req, res)=>{
            image,
            verifyEmail: false,
          })
+         console.log(process.env.JWT_SECRET)
 
          // generate a verify token
-         const verifyToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRETE || "secretkey", { expiresIn: '30d' })
-
+         const verifyToken = jwt.sign(
+           { userId: user._id },
+           process.env.JWT_SECRETE || 'thisisasecret',
+           { expiresIn: '30d' }
+         )
+         
          // send the verify email
-         const mailOption = {
-           from: process.env.EMAIL_USER,
-           to: email,
-           subject: 'Verify your email',
-           html: `<p> Please click the following link to verify your email: </p>
-            <a href="http://localhost:3000/verify-email/${verifyToken}">Verify Email</a>`,
-         }
+        const mailOption = {
+          from: process.env.EMAIL_USER,
+          to: email,
+          subject: 'Verify your email___',
+          html: `<p>Please click the following link to verify your email:</p>
+         <a href="http://localhost:8001/api/v1/verify-email?token=${verifyToken}">Verify Email</a>`,
+        }
 
          await sendEmail(mailOption)
 
@@ -63,6 +68,44 @@ const userSignup = async (req, res)=>{
     }
 }
 
-module.exports ={
-    userSignup
+const verifyEmail = async (req,res)=>{
+    const { token } = req.query
+    console.log("this is the token__",token)
+
+    try {
+        // verify the token
+        const decoded = jwt.verify(
+          token,
+          process.env.JWT_SECRETE || 'thisisasecret'
+        )
+        const userId = decoded.userId
+
+        // update the verifyEmail 
+        const user = await User.findByIdAndUpdate(userId, {verifyEmail: true}, {new: true})
+        console.log(user)
+        if(!user){
+            return res.status(404).json({
+                status: false,
+                message: 'User not found',
+            })
+
+        }
+
+         res.redirect(
+           `${process.env.FONTEND_URL}/profile-setup?type=${user.accountType}`
+         )
+         console.log('this is the user', user.accountType)
+
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: 'Something went wrong',
+            error: error.message,
+        })
+    }
+}
+
+module.exports = {
+  userSignup,
+  verifyEmail,
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { userSignup } = require("../controllers/user.controller");
+const { userSignup, verifyEmail } = require('../controllers/user.controller')
 const router = express.Router();
  
 router.post("/signup", userSignup);
+router.get('/verify-email/', verifyEmail)
 
 module.exports = router;
