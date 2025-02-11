b15937d30fc05eea2bdfe81164c1bb537faf516c
saif9795 <adnan.saif9795@gmail.com>
2025-02-11 12:27:38 +0600

    routes and controllers for professional has been added

---
+++ models/merchantServices.model.js
@@ -0,0 +1,132 @@
+const mongoose = require('mongoose');
+
+const merchantServicesSchema = new mongoose.Schema(
+  {
+    merchantID:{
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    QR:{
+      type: String,
+    },
+    referNumber:{
+      type: Number,
+    },
+    serviceName: {
+      type: String,
+    },
+    metaDescription: {
+      type: String,
+    },
+    serviceDescription: {
+      type: String,
+    },
+    keyWords: [
+      {
+        type: String,
+      }
+    ],
+    paymentType:[
+      {
+        type: String,
+      }
+    ],
+    price:{
+      type:Number,
+    },
+    serviceImage: {
+      type: String,
+    },
+    serviceVideo: {
+      type: String,
+    },
+    date:{
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    sessionType:{
+      type: String,
+    },
+    statusType:{
+      type: String,
+      enum:["confirmed","pending","cancelled"]
+    },
+    orderSlug:{
+      type: String,
+    },
+    FAQ:[
+      {
+        professionalServiceID: {
+          type: mongoose.Types.ObjectId,
+          ref: "FAQ",
+        },
+      }
+    ],
+    review:[
+      {
+        professionalServiceID: {
+          type: mongoose.Types.ObjectId,
+          ref: "Review",
+        },
+      }
+    ],
+    support:[
+      {
+        supportID: {
+          type: mongoose.Types.ObjectId,
+          ref: "Support",
+        }
+      }
+    ],
+    policy: [
+      {
+        beforeAppointment: {
+          type: String,
+        },
+        afterAppointment: {
+          type: String,
+        },
+        cancellationWindow: {
+          type: String,
+        },
+        noShowPolicy: {
+          type: String,
+        },
+      }
+    ],
+    aboutMe: {
+      type: String,
+    },
+    highlightedStatement: [
+      {
+        type: String,
+      }
+    ],
+    experience: [
+      {
+        type: String,
+      }
+    ],
+    certifications: [
+      {
+        type: String,
+      }
+    ],
+    goLive:[
+      {
+        goLive: {
+          type: mongoose.Types.ObjectId,
+          ref: "Golive",
+        }
+      }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Merchantservices = mongoose.model("Merchantservices", merchantServicesSchema);
+module.exports = Merchantservices;
