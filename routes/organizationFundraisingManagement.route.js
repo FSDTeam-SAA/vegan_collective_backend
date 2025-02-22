2e7980638a3584b0195b81da10ae299c3064233b
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-02-22 22:38:06 +0600

    organizatin fund

--- routes/organizationFundraisingManagement.route.js
+++ routes/organizationFundraisingManagement.route.js
@@ -1,4 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const organizationFundraisingManagementController = require("../controllers/organizationFundraisingManagement.controller");
 
-module.exports = router
+// Create a new fundraising campaign
+router.post("/campaigns", organizationFundraisingManagementController.createCampaign);
+
+// Get all fundraising campaigns
+router.get("/campaigns", organizationFundraisingManagementController.getAllCampaigns);
+
+// Get a single fundraising campaign by ID
+router.get("/campaigns/:id", organizationFundraisingManagementController.getCampaignById);
+
+// Update a fundraising campaign by ID
+router.put("/campaigns/:id", organizationFundraisingManagementController.updateCampaign);
+
+// Delete a fundraising campaign by ID
+router.delete("/campaigns/:id", organizationFundraisingManagementController.deleteCampaign);
+
+module.exports = router;
