adc4f8e22f7138292fb15d80d9558079fbba075e
zihadulIslam2 <zihadul708@gmail.com>
2025-02-24 17:57:53 +0600

    Add additional model imports for global find functionality

--- controllers/globalfind.js
+++ controllers/globalfind.js
@@ -1,8 +1,41 @@
 const User = require('../models/user.model'); // Import User model
+const commentManipulation = require('../models/commentManipulation.model');
+const Support = require('../models/support.model');
+// Merchant models
+const Merchantinfo = require('../models/merchantInfo.model')
+const MerchantPolicies = require('../models/merchantPolicies.model')
+const Merchantproductdelivary = require('../models/merchantProductDelivary.model')
+const MerchantProducts = require('../models/merchantProducts.model');
+const Merchantproductsreview = require('../models/merchantProductsReview.model')
+const Merchantsalesmanagements = require('../models/merchantSalesManagement.model')
+const Merchantsupport = require('../models/merchantSupport.model')
+const Merchantgolive = require('../models/merchantGoLive.model')
+const Merchantcustomermanagement = require('../models/merchantCustomerManagement.model')
+
+// organization
+const Organizationeventbooking = require('../models/organizationEventBooking.model')
+const Organizationeventmanagements = require('../models/organizationEventManagement.model')
+const Organizationfundraisingmanagements = require('../models/organizationFundraisingManagement.model')
+const Organizationgolives = require('../models/organizationGoLive.model')
+const Organizationinfos = require('../models/organizationInfo.model')
+const Organizationsupports = require('../models/organizationSupport.model')
+const Organizationupdateandnews = require('../models/organizationUpdateAndNews.model')
+
+// Professional models
+const ProfessionalFAQ = require('../models/professionalFAQ.model')
+const ProfessionalFAQ = require('../models/professionalFAQ.model')
+const Professionalpolicies = require('../models/professionalPolicies.model')
+const Professionalservicebookings = require('../models/professionalServiceBooking.model')
+const Professionalservices = require('../models/professionalServices.model')
+
+const Userpayments = require('../models/userPayment.model')
+const Users = require('../models/user.model')
+
+
 
 exports.findByAccountTypeOrId = async (req, res) => {
   try {
-    const { accountType, userID } = req.query; // Get query params
+    const { accountType, userID } = req.query; 
 
     // Validate accountType
     if (!accountType || !userID) {
@@ -42,3 +75,4 @@ exports.findByAccountTypeOrId = async (req, res) => {
     });
   }
 };
+ 
--- routes/globalfind.js
+++ routes/globalfind.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const globalFindController = require('../controllers/globalfind'); // Import the controller
-
+ 
 
 // GET /api/globalfind?accountType=merchant
 // GET /api/globalfind?accountType=merchant&userID=64f1b2c8e4b0f5a2d8f7e1a2
