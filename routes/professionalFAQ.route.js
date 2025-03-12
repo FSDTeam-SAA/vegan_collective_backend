9d7af57be35718f45d2afef45fb37726df60b50e
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-03-12 09:03:26 +0600

    Refactor FAQ routes and controller to use User ID for fetching, updating, and deleting FAQs

--- routes/professionalFAQ.route.js
+++ routes/professionalFAQ.route.js
@@ -9,12 +9,12 @@ router.post("/faqs", professionalFAQController.createFAQ);
 router.get("/faqs", professionalFAQController.getAllFAQs);
 
 // Get a specific FAQ by ID
-router.get("/faqs/:id", professionalFAQController.getFAQByID);
+router.get("/faqs/:userID", professionalFAQController.getFAQsByUserID);
 
 // Update an FAQ by ID
-router.put("/faqs/:id", professionalFAQController.updateFAQ);
+router.put("/faqs/:userID", professionalFAQController.updateFAQsByUserID);
 
 // Delete an FAQ by ID
-router.delete("/faqs/:id", professionalFAQController.deleteFAQ);
+router.delete("/faqs/:userID", professionalFAQController.deleteFAQsByUserID);
 
 module.exports = router;
