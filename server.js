4a1cb1a4a2d1b7e46ab943869e6ab7bc81d36f22
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-03-19 14:37:21 +0600

    Add newsletter subscription and management functionality

--- server.js
+++ server.js
@@ -87,6 +87,8 @@ const googleAuthRoute = require("./routes/googleAuth.js")
 const checkUserPaymentRoute = require("./routes/checkUserPayment.route.js")
 const userPaymentDetailsRoute = require("./routes/userPaymentDetails.route.js")
 
+const newsletterRoutes = require("./routes/newsletterRoutes");
+
 
 //endpoints for professional
 app.use("/api/v1", professionalBooking);
@@ -160,6 +162,9 @@ app.use('/api/v1', googleAuthRoute)
 
 //volunteer routes
 app.use("/api/v1", organizationVolunteerRoutes);
+
+//newsletter routes
+app.use("/api/v1", newsletterRoutes);
 
 //event routes
 app.use("/api/v1", eventRoutes);
