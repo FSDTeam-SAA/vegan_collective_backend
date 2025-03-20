7255aea26119eff9816b0c182bd926cd50dcbf0c
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-03-20 20:41:06 +0600

    Refactor monthly sales and earnings calculation

--- routes/merchantGraph.route.js
+++ routes/merchantGraph.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const getMonthlySalesAndEarnings = require('../controllers/merchantGraph.controller');
 const router = express.Router();
 
-router.get("/get/merchant/graph", getMonthlySalesAndEarnings);
+router.get("/get/merchant/graph/:userID", getMonthlySalesAndEarnings);
 
 module.exports = router;
