bd68348be8e408fdd4175dbfb768c39da1fa262e
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-04-07 11:35:29 +0600

    Add unique eventId field to merchantGoLive model for better identification

--- models/merchantGoLive.model.js
+++ models/merchantGoLive.model.js
@@ -1,42 +1,48 @@
 const mongoose = require('mongoose');
 
 const merchantGoLiveSchema = new mongoose.Schema(
-  {
-    merchantID: {
-      type: mongoose.Types.ObjectId,
-      ref: 'User',
-     
-    },
-    eventTitle: {
-      type: String
-     
-    },
-    description: {
-      type: String,
-    },
-    date: {
-      type: String,
-      required: true,
-    },
-    time: {
-      type: String,
-      required: true,
-    },
-    eventType: {
-      type: String,
-      enum: ['paid event', 'free event'],
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: function () {
-        return this.eventType === 'paid event';
-      },
-    },
-  },
-  {
-    timestamps: true,
-  }
+{
+merchantID: {
+type: mongoose.Types.ObjectId,
+ref: 'User',
+
+},
+eventTitle: {
+type: String,
+
+},
+description: {
+type: String,
+},
+date: {
+type: String,
+required: true,
+},
+time: {
+type: String,
+required: true,
+},
+eventType: {
+type: String,
+enum: ['paid event', 'free event'],
+required: true,
+},
+price: {
+type: Number,
+required: function () {
+return this.eventType === 'paid event';
+},
+},
+eventId: { 
+type: String,
+unique: true,
+default: () => new mongoose.Types.ObjectId().toString(), // Generate a unique ID
+},
+
+},
+{
+timestamps: true,
+}
 );
 
 const Merchantgolive = mongoose.model('Merchantgolive', merchantGoLiveSchema);
