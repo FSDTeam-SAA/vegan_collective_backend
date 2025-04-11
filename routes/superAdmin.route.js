0c127ac6086d274fdd4d740926ba06b1283ec012
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-04-11 10:47:21 +0600

    developed: add vendor verification counts endpoint for super admin

--- routes/superAdmin.route.js
+++ routes/superAdmin.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { verifierCreate, getAllUsersForSuperAdmin, getAllMerchantsForSuperAdmin, getAllProfessionalsForSuperAdmin, getAllOrganizationsForSuperAdmin, getAllVerifiersForSuperAdmin } = require("../controllers/superAdmin.controller");
+const { verifierCreate, getAllUsersForSuperAdmin, getAllMerchantsForSuperAdmin, getAllProfessionalsForSuperAdmin, getAllOrganizationsForSuperAdmin, getAllVerifiersForSuperAdmin,getVendorVerificationCounts } = require("../controllers/superAdmin.controller");
 const router = express.Router();
 
 //super admin can make change the role of user to verifier
@@ -17,7 +17,7 @@ router.get("/superadmin/getAllProfessionals", getAllProfessionalsForSuperAdmin);
 //super admin can get all organizations with pagination and filter
 router.get("/superadmin/getAllOrganizations", getAllOrganizationsForSuperAdmin);
 
-//super admin can get all verifiers with pagination and filter
-router.get("/superadmin/getAllVerifiers", getAllVerifiersForSuperAdmin);
+//super admin can get all verifiers 
+router.get("/superadmin/getVendorVerificationCounts", getVendorVerificationCounts);
 
 module.exports = router;
